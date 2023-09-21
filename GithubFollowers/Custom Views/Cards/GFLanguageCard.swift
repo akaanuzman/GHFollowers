e4c37597a02f581338a779e0584f1d6cc7064d54
Kaan Uzman <55639112+akaanuzman@users.noreply.github.com>
2023-09-21 03:48:54 +0300

    language card ui completed

---
+++ GithubFollowers/Custom Views/Cards/GFLanguageCard.swift
@@ -0,0 +1,20 @@
+//
+//  GFLanguageCard.swift
+//  GithubFollowers
+//
+//  Created by Kaan Uzman on 21/9/23.
+//
+
+import UIKit
+
+class GFLanguageCard: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
